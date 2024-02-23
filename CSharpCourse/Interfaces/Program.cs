2f675412fc032d4205e1a942af02055c36499b07
doathe <nisa.tur@gmail.com>
2024-02-23 17:56:13 +0300

    Interfaces added

---
+++ CSharpCourse/Interfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+public interface IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+public class Customer : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+}
+public class Student : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Department { get; set; }
+}

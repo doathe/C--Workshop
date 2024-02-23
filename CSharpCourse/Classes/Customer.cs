238a22f8cba94f987157c3ebc619b93c3bf749a4
doathe <nisa.tur@gmail.com>
2024-02-23 16:38:09 +0300

    Classes updated

---
+++ CSharpCourse/Classes/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace Classes;
+
+public class Customer
+{
+
+
+    //// Encapsulation
+    //private string _firstName;
+    //public string FirstName { 
+    //    get{ return "Mr. " + _firstName; }
+    //    set { _firstName = value; }
+    //}
+
+    // Property
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+
+    // Field
+    public string Region;
+}
--- CSharpCourse/Classes/Program.cs
+++ CSharpCourse/Classes/Program.cs
@@ -9,3 +9,19 @@ ProductManager productManager = new ProductManager();
 
 productManager.Add();
 productManager.Update();
+
+Customer customer = new Customer();
+customer.City = "Ankara";
+customer.Id = 1;
+customer.FirstName = "Doğa";
+customer.LastName = "Turhan";
+
+Customer customer2 = new Customer 
+{ 
+    Id = 2,
+    FirstName = "Derin",
+    LastName = "Demiroğ",
+    City = "İstanbul",
+};
+
+Console.WriteLine(customer2.FirstName);

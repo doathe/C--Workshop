cbb06c22c884d597283d232603c17b62067d3d03
doathe <nisa.tur@gmail.com>
2024-02-23 11:28:40 +0300

    Loops updated

--- CSharpCourse/Loops/Program.cs
+++ CSharpCourse/Loops/Program.cs
@@ -35,4 +35,28 @@ foreach (string student in students)
     Console.WriteLine(student);
 }
 
+// Prime number example
+static bool IsPrimeNumber(int number)
+{
+    bool result = true;
+    for(int i = 2; i < number-1; i++)
+    {
+        if (number % i == 0)
+        {
+            result = false;
+            i = number;
+        }
+    }
+    return result;
+}
+
+if (IsPrimeNumber(10))
+{
+    Console.WriteLine("This is a prime number");
+}
+else
+{
+    Console.WriteLine("This is not a prime number");
+}
+
 Console.ReadLine();
